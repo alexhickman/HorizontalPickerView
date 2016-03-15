f7722b72f80165e233525aa56665c14e841b261e
alexhickman <alex.hickman3@gmail.com>
2016-03-14 17:53:16 -0700

    Initial Commit

---
+++ HorizontalPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalPickerView
+//
+//  Created by Hickman on 3/14/16.
+//  Copyright © 2016 Hickman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorizontalPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorizontalPickerView
+//
+//  Created by Hickman on 3/14/16.
+//  Copyright © 2016 Hickman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
